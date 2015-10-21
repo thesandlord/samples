de7f7b917a1188f655262d394655a5bd7155e581
Sandeep Dinesh <sandeepdinesh@google.com>
2015-10-21 15:01:57 -0700

    Updated Datastore example to work with latest gcloud-python, and using datetime instead of strings for timestamp

--- google-cloud-datastore/interactive-demo/code.py
+++ google-cloud-datastore/interactive-demo/code.py
@@ -19,16 +19,13 @@
 from gcloud import datastore
 import os
 import time
+import datetime
 
 #The next few lines will set up your environment variables
 #Replace "YOUR_RPOEJCT_ID_HERE" with the correct value in code.py
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "key.json"
+projectID = "smart-spark-93622"
 
-projectID = "YOUR_RPOEJCT_ID_HERE"
-
-os.environ["GCLOUD_TESTS_PROJECT_ID"] = projectID
-os.environ["GCLOUD_TESTS_DATASET_ID"] = projectID
-datastore.set_default_dataset_id(projectID)
+client = datastore.Client.from_service_account_json( json_credentials_path="key.json", dataset_id=projectID )
 
 #Let us build a message board / news website
 
@@ -36,7 +33,7 @@ datastore.set_default_dataset_id(projectID)
 email = "me@fake.com"
 
 #Now, create a 'key' for that user using the email
-user_key = datastore.Key('User', email)
+user_key = client.key('User', email)
 print( user_key )
 
 #Now create a entity using that key
@@ -50,39 +47,39 @@ new_user["email"] = unicode(email)
 print( new_user )
 
 #Push entity to the Cloud Datastore
-datastore.put( new_user )
+client.put( new_user )
 
 #Get the user from datastore and print
-print( datastore.get(user_key) )
+print( client.get(user_key) )
 
 #Make a new incomplete key for the story
 #datastore will make a unique id automatically
-story_key = datastore.Key( "Story" )
+story_key = client.key( "Story" )
 new_story = datastore.Entity( key = story_key )
 
 #Add some fields
 new_story["url"] = unicode("cloud.google.com")
 new_story["title"] = unicode("Google is an awesome cloud provider")
 new_story["text"] = unicode("Check out this cool website I found for Cloud Stuff!")
-new_story['timestamp'] = unicode(time.strftime("%s", time.gmtime()))
+new_story['timestamp'] = datetime.datetime.now()
 print( new_story )
 
 #Push entity to the Cloud Datastore
-datastore.put( new_story )
+client.put( new_story )
 
 #Query for the stories
 #This query is eventually consistant
-query = datastore.Query(kind='Story', order=["-timestamp"])
+query = datastore.Query( client=client, kind='Story', order=["-timestamp"])
 results = list( query.fetch() )
 print( results )
 
 #Make another story
-new_story = datastore.Entity( key = datastore.Key("Story") )
+new_story = datastore.Entity( key = client.key("Story") )
 new_story["url"] = unicode("blog.sandeepdinesh.com")
 new_story["title"] = unicode("Interesting Blog")
 new_story["text"] = unicode("This is a pretty cool blog I write. Check it out!")
-new_story['timestamp'] = unicode(time.strftime("%s", time.gmtime()))
-datastore.put( new_story )
+new_story['timestamp'] = datetime.datetime.now()
+client.put( new_story )
 
 #Query for the stories
 results = list(query.fetch())
@@ -97,15 +94,15 @@ print( story_id )
 
 #Create the comment
 #The ancestor (parent) is the user
-comment_key = datastore.Key('Comment', parent=user_key)
+comment_key = client.key('Comment', parent=user_key)
 new_comment = datastore.Entity( key=comment_key )
 new_comment['text'] = unicode("Cool story bro")
 new_comment['storyid'] = unicode(story_id)
-new_comment['timestamp'] = unicode(time.strftime("%s", time.gmtime()))
+new_comment['timestamp'] = datetime.datetime.now()
 print( new_comment )
 
 #Push comment to datastore
-datastore.put( new_comment )
+client.put( new_comment )
 
 #Get the id from the second story
 story = results[1]
@@ -113,22 +110,22 @@ story_id = story.key.id_or_name
 print( story_id )
 
 #Create another comment
-new_comment = datastore.Entity( key=datastore.Key('Comment', parent=user_key) )
+new_comment = datastore.Entity( key=client.key('Comment', parent=user_key) )
 new_comment['text'] = unicode("Wow. Much Awesome. So Rad.")
 new_comment['storyid'] = unicode(story_id)
-new_comment['timestamp'] = unicode(time.strftime("%s", time.gmtime()))
+new_comment['timestamp'] = datetime.datetime.now()
 print( new_comment )
 
 #Push comment to datastore
-datastore.put( new_comment )
+client.put( new_comment )
 
 #Query for all comments
-query = datastore.Query( kind='Comment', order=["timestamp"] )
+query = datastore.Query(client=client, kind='Comment', order=["timestamp"] )
 results = list( query.fetch() )
 print( results )
 
 #Query for comments on a single story
-query = datastore.Query( kind='Comment', filters=[( 'storyid', '=', unicode(story_id) )], order=["timestamp"] )
+query = datastore.Query(client=client, kind='Comment', filters=[( 'storyid', '=', unicode(story_id) )], order=["timestamp"] )
 
 #Query will fail without a joint index on storyid and timestamp!
 
@@ -138,25 +135,25 @@ print( results )
 
 #Let's make a new user and comment on the same story
 email = "msfake@fake.com"
-second_user = datastore.Entity( key=datastore.Key('User', email) )
+second_user = datastore.Entity( key=client.key('User', email) )
 second_user["name"] = unicode("Shesa Fakealso")
 second_user["email"] = unicode(email)
-datastore.put( second_user )
+client.put( second_user )
 
-new_comment = datastore.Entity( key=datastore.Key('Comment', parent=datastore.Key('User', email) ) )
+new_comment = datastore.Entity( key=client.key('Comment', parent=client.key('User', email) ) )
 new_comment['text'] = unicode("Boring story :( ")
 new_comment['storyid'] = unicode(story_id)
-new_comment['timestamp'] = unicode(time.strftime("%s", time.gmtime()))
-datastore.put( new_comment )
+new_comment['timestamp'] = datetime.datetime.now()
+client.put( new_comment )
 
 #Query again
-query = datastore.Query( kind='Comment', filters=[( 'storyid', '=', unicode(story_id) )], order=["timestamp"] )
+query = datastore.Query(client=client, kind='Comment', filters=[( 'storyid', '=', unicode(story_id) )], order=["timestamp"] )
 results = list( query.fetch() )
 print( results )
 
 #Perform strongly consistant query
 #Use the user_key as the ancestor to get the users comments
-query = datastore.Query( kind='Comment', ancestor=user_key )
+query = datastore.Query(client=client, kind='Comment', ancestor=user_key )
 results = list( query.fetch() )
 print( results )
 
