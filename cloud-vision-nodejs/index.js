73a881365a557ab1844347c17de68b3165b63dc0
Sandeep Dinesh <sandeepdinesh@google.com>
2016-04-07 10:41:26 -0700

    Cleaned code to fit Google standards

--- cloud-vision-nodejs/index.js
+++ cloud-vision-nodejs/index.js
@@ -14,17 +14,19 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-var express = require("express");
+'use strict';
+
+var express = require('express');
 var fs = require('fs');
-var util = require("util");
-var mime = require("mime");
+var util = require('util');
+var mime = require('mime');
 var multer = require('multer');
-var upload = multer({ dest: 'uploads/' });
+var upload = multer({dest: 'uploads/'});
 
 // Set up auth
 var gcloud = require('gcloud')({
-	keyFilename: 'key.json',
-	projectId: '<YOUR-PROJECT-ID-HERE>'
+  keyFilename: 'key.json',
+  projectId: '<YOUR-PROJECT-ID-HERE>'
 });
 
 var vision = gcloud.vision();
@@ -32,52 +34,57 @@ var vision = gcloud.vision();
 var app = express();
 
 // Simple upload form
-var form = "<!DOCTYPE HTML><html><body>" +
-"<form method='post' action='/upload' enctype='multipart/form-data'>" +
-"<input type='file' name='image'/>" +
-"<input type='submit' /></form>" +
-"</body></html>";
+var form = '<!DOCTYPE HTML><html><body>' +
+  "<form method='post' action='/upload' enctype='multipart/form-data'>" +
+  "<input type='file' name='image'/>" +
+  "<input type='submit' /></form>" +
+  '</body></html>';
 
-app.get('/', function (req, res){
-	res.writeHead(200, {'Content-Type': 'text/html' });
-	res.end(form);	
+app.get('/', function(req, res) {
+  res.writeHead(200, {
+    'Content-Type': 'text/html'
+  });
+  res.end(form);
 });
 
 // Get the uploaded image
 // Image is uploaded to req.file.path
-app.post('/upload', upload.single('image'), function (req, res, next) {
-	
-	// Choose what the Vision API should detect
-	// Choices are: faces, landmarks, labels, logos, properties, safeSearch, texts
-	var types = ["labels"]
-	
-	// Send the image to the Cloud Vision API
-	vision.detect(req.file.path, types, function(err, detections, apiResponse) {
-		if(err){
-			res.end("Cloud Vision Error");
-		} else {
-			res.writeHead(200, {'Content-Type': 'text/html' });
-			res.write('<!DOCTYPE HTML><html><body>');
-			
-			// Base64 the image so we can display it on the page
-			res.write('<img width=200 src="' + base64Image(req.file.path) + '"><br>');
-			
-			// Write out the JSON output of the Vision API
-			res.write(JSON.stringify(detections, null, 4));
-			
-			// Delete file (optional)
-			fs.unlinkSync(req.file.path);
-			
-			res.end('</body></html>');
-		}
-	});
+app.post('/upload', upload.single('image'), function(req, res, next) {
+
+  // Choose what the Vision API should detect
+  // Choices are: faces, landmarks, labels, logos, properties, safeSearch, texts
+  var types = ['labels'];
+
+  // Send the image to the Cloud Vision API
+  vision.detect(req.file.path, types, function(err, detections, apiResponse) {
+    if (err) {
+      res.end('Cloud Vision Error');
+    } else {
+      res.writeHead(200, {
+        'Content-Type': 'text/html'
+      });
+      res.write('<!DOCTYPE HTML><html><body>');
+
+      // Base64 the image so we can display it on the page
+      res.write('<img width=200 src="' + base64Image(req.file.path) + '"><br>');
+
+      // Write out the JSON output of the Vision API
+      res.write(JSON.stringify(detections, null, 4));
+
+      // Delete file (optional)
+      fs.unlinkSync(req.file.path);
+
+      res.end('</body></html>');
+    }
+  });
 });
 
 app.listen(8080);
-console.log("Server Started");
+console.log('Server Started');
 
 // Turn image into Base64 so we can display it easily
+
 function base64Image(src) {
-	var data = fs.readFileSync(src).toString("base64");
-	return util.format("data:%s;base64,%s", mime.lookup(src), data);
+  var data = fs.readFileSync(src).toString('base64');
+  return util.format('data:%s;base64,%s', mime.lookup(src), data);
 }
